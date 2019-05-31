b3ea778564250942caa874b6c83f1b8ae16712b4
jleatham <j.leatham@gmail.com>
2019-05-31 15:56:54 -0500

    gif test

--- botFunctions.py
+++ botFunctions.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 TACO_EMAIL = os.environ['TACO_EMAIL']
@@ -11,3 +12,17 @@ TEST_EMAIL_LIST_DALLAS = os.environ['TEST_EMAIL_LIST_DALLAS']
 TEST_EMAIL_LIST_AUSTIN = os.environ['TEST_EMAIL_LIST_AUSTIN']
 PROCESSED_EMAIL_LIST_DALLAS = TEST_EMAIL_LIST_DALLAS.split(' ')
 PROCESSED_EMAIL_LIST_AUSTIN = TEST_EMAIL_LIST_AUSTIN.split(' ')
+
+def bot_send_gif(room_id, gif):
+    """
+        Sends the email file generated using MultipartEncoder which
+        breaks the requests into tiny peices to get past the 5K Teams limit
+
+    """
+    m = MultipartEncoder({'roomId': room_id,
+                      'text': 'Taco Time!',
+                      'files': , gif})
+    r = requests.post('https://api.ciscospark.com/v1/messages', data=m,
+                    headers={'Authorization': os.environ['TACO_TOKEN'],
+                    'Content-Type': m.content_type})
+    return r.text
--- myScheduler.py
+++ myScheduler.py
@@ -1,5 +1,5 @@
 from myhug import TACO_HEADERS, bot_post_to_room, URL
-from botFunctions import TEST_ROOM_ID, PROCESSED_EMAIL_LIST_DALLAS, PROCESSED_EMAIL_LIST_AUSTIN
+from botFunctions import TEST_ROOM_ID, PROCESSED_EMAIL_LIST_DALLAS, PROCESSED_EMAIL_LIST_AUSTIN, bot_send_gif
 import random
 import requests
 import json
@@ -9,4 +9,4 @@ random_dallas = random.choice(PROCESSED_EMAIL_LIST_DALLAS)
 random_austin = random.choice(PROCESSED_EMAIL_LIST_AUSTIN)
 
 bot_post_to_room(TEST_ROOM_ID,f"<@personEmail:jleatham@cisco.com|{random_dallas}>:  Taco Time! ",TACO_HEADERS)
-
+bot_send_gif(TEST_ROOM_ID,'https://media.giphy.com/media/WNs0uptipSG40/giphy.gif')
