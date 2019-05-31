632527a27c5944eb95e51434d82bf755296c4906
jleatham <j.leatham@gmail.com>
2019-05-31 15:28:23 -0500

    scheduler test

--- myScheduler.py
+++ myScheduler.py
@@ -2,6 +2,7 @@ from myhug import TACO_HEADERS, bot_post_to_room, URL
 from botFunctions import TEST_ROOM_ID, PROCESSED_EMAIL_LIST_DALLAS, PROCESSED_EMAIL_LIST_AUSTIN
 import random
 import requests
+import json
 print("hello test scheduler")
 
 random_dallas = random.choice(PROCESSED_EMAIL_LIST_DALLAS)
