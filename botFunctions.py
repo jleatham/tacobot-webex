8805e459fdaac1c152460b3ebcda91f3fc9d9bc0
jleatham <j.leatham@gmail.com>
2019-05-31 21:48:35 -0500

    gif test

--- botFunctions.py
+++ botFunctions.py
@@ -14,15 +14,15 @@ PROCESSED_EMAIL_LIST_DALLAS = TEST_EMAIL_LIST_DALLAS.split(' ')
 PROCESSED_EMAIL_LIST_AUSTIN = TEST_EMAIL_LIST_AUSTIN.split(' ')
 
 def bot_send_gif(room_id, gif):
-    """
-        Sends the email file generated using MultipartEncoder which
-        breaks the requests into tiny peices to get past the 5K Teams limit
-
-    """
-    m = MultipartEncoder({'roomId': room_id,
-                      'text': 'Taco Time!',
-                      'files': (gif,gif,'image/gif')})
-    r = requests.post('https://api.ciscospark.com/v1/messages', data=m,
-                    headers={'Authorization': os.environ['TACO_TOKEN'],
-                    'Content-Type': m.content_type})
-    return r.text
+    #try to post
+    payload = {"roomId": room_id,
+               "markdown": message,
+               "files":[gif]}
+    response = requests.request("POST", URL, data=json.dumps(payload), headers=headers)
+    #error handling
+    if response.status_code != 200:
+        #modify function to receive user_input as well so we can pass through
+        #user_input = "some test message for the moment"
+        #send to the DEVs bot room
+        #error_handling(response,response.status_code,user_input,room_id,headers)
+        print("error posting to room")
--- myScheduler.py
+++ myScheduler.py
@@ -3,10 +3,13 @@ from botFunctions import TEST_ROOM_ID, PROCESSED_EMAIL_LIST_DALLAS, PROCESSED_EM
 import random
 import requests
 import json
+
+#chron runs every day at 8AM, so this needs to check if it is Thursday, and only run then.
 print("hello test scheduler")
 
 random_dallas = random.choice(PROCESSED_EMAIL_LIST_DALLAS)
 random_austin = random.choice(PROCESSED_EMAIL_LIST_AUSTIN)
 
-bot_post_to_room(TEST_ROOM_ID,f"<@personEmail:jleatham@cisco.com|{random_dallas}>:  Taco Time! ",TACO_HEADERS)
+
+bot_post_to_room(TEST_ROOM_ID,f"<@personEmail:jleatham@cisco.com|{random_dallas}> and <@personEmail:jleatham@cisco.com|{random_austin}>:  You're on deck",TACO_HEADERS)
 bot_send_gif(TEST_ROOM_ID,'https://media.giphy.com/media/WNs0uptipSG40/giphy.gif')
