5dfd8555aa97a3692d43520d11965bdd8d88bcb6
jleatham <j.leatham@gmail.com>
2019-06-03 12:19:30 -0500

    getting ready to deploy v0

--- botFunctions.py
+++ botFunctions.py
@@ -25,23 +25,17 @@ TACO_HEADERS = {
 }
 
 
-
-TACO_GIF_LIST = ['https://media.giphy.com/media/WNs0uptipSG40/giphy.gif',
-            'https://media.giphy.com/media/pYCdxGyLFSwgw/giphy.gif',
-            'https://media.giphy.com/media/EHCNlAWDPcOME/giphy.gif',
-            'https://media.giphy.com/media/26tnlYdWMzIYdaocU/giphy.gif',
-            'https://media.giphy.com/media/kGK4VeSwqEfbW/giphy.gif',
-            'https://media.giphy.com/media/pxXV5nDJhHthm/giphy.gif',
-            'https://media.giphy.com/media/3o7ZeT4XKYLG6x8zqo/giphy.gif',
-            'https://media.giphy.com/media/3o7ZezGPktFNZj93os/giphy.gif',
-            'https://media.giphy.com/media/3o6ZtkmiFtpBvii6uQ/giphy.gif']
-
-TACO_MESSAGE = ['Taco Time!',
-                'All meetings are better with Tacos',
-                "♫ Frozen ♫  -- ♫ Do you want to bring a Taco? ♫ ... ♫ It doesn't have to be a Taco ♫ ",
-                'You cannot make everybody happy, you are not a Taco.',
-                'Surround yourself with Tacos, not negativity.',
-                'Everybody loves Tacos']
+TACO_MESSAGE = [
+                 ['https://media.giphy.com/media/WNs0uptipSG40/giphy.gif',      'Everybody gets a TACO!'],
+                 ['https://media.giphy.com/media/pYCdxGyLFSwgw/giphy.gif',      "It's raining Tacos!"],
+                 ['https://media.giphy.com/media/EHCNlAWDPcOME/giphy.gif',      'Taco Time!'],
+                 ['https://media.giphy.com/media/26tnlYdWMzIYdaocU/giphy.gif',  'You cannot make everybody happy, you are not a Taco.'],
+                 ['https://media.giphy.com/media/kGK4VeSwqEfbW/giphy.gif',      'All meetings are better with Tacos'],
+                 ['https://media.giphy.com/media/pxXV5nDJhHthm/giphy.gif',      "♫ Do you want to bring a Taco? ♫ ... ♫ It doesn't have to be a Taco ♫ "],
+                 ['https://media.giphy.com/media/3o7ZeT4XKYLG6x8zqo/giphy.gif', 'Surround yourself with Tacos, not negativity.'],
+                 ['https://media.giphy.com/media/3o7ZezGPktFNZj93os/giphy.gif', 'It is only Tacos all the way down'],
+                 ['https://media.giphy.com/media/3o6ZtkmiFtpBvii6uQ/giphy.gif', 'Everybody loves Tacos']
+                ]
 
 def bot_send_gif(room_id, gif, message):
     #try to post
@@ -60,11 +54,10 @@ def bot_send_gif(room_id, gif, message):
 def NTX_TACO_SELECTOR(room_id):    
     random_dallas = random.choice(PROCESSED_EMAIL_LIST_DALLAS)
     random_austin = random.choice(PROCESSED_EMAIL_LIST_AUSTIN)
-    random_taco_gif = random.choice(TACO_GIF_LIST)
     random_taco_messsage = random.choice(TACO_MESSAGE)
 
     bot_post_to_room(room_id,f"<@personEmail:{random_dallas}@cisco.com|{random_dallas}> and <@personEmail:{random_austin}@cisco.com|{random_austin}>:  You're on deck to bring Tacos!",TACO_HEADERS)
-    bot_send_gif(room_id,random_taco_gif, random_taco_messsage)
+    bot_send_gif(room_id,random_taco_messsage[0], random_taco_messsage[1])
 
 
 
